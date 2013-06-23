f93b9d00e747a36fdd975eeef0b8006c9e21ed4b
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-06-23 00:20:13 -0700

    code example

--- example_test.go
+++ example_test.go
@@ -1,5 +1,34 @@
 package jsontree
 
+import (
+	"fmt"
+)
+
 func Example() {
-	// Output:
+	raw := `{
+		"null":   null,
+		"object": {
+			"int":   123,
+			"bool":  false,
+			"array": [
+				"string",
+				null
+			]
+		}
+	}`
+	tree := New()
+	err := tree.UnmarshalJSON([]byte(raw))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tree.Get("object").Get("array").GetIndex(0).String())
+	fmt.Println(tree.Get("object").Get("int").Number())
+	fmt.Println(tree.Get("doesntexist").Err())
+	fmt.Println(tree.Get("null").Err())
+	fmt.Println(tree.Get("null").IsNull())
+	// Output: string <nil>
+	// 123 <nil>
+	// key does not exist; $.doesntexist
+	// <nil>
+	// true
 }
